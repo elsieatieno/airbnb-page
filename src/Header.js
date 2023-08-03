b2efd8143657ab3c32d7aff9ffd9f9e2d35c3ed9
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-03 13:19:38 +0300

    Structured component

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import LanguageIcon from '@mui/icons-material/Language';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img className='header_icon' src='https://i.pinimg.com/originals/3c/
+      bf/be/3cbfbe148597341fa56f2f87ade90956.png' alt="logo" />
+      <div className='header_center'>
+        <input type='text' />
+        <SearchIcon />
+      </div>
+      <div className='header_right'>
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <AccountCircleIcon />
+      </div>
+    </div>
+  )
+}
+
+export default Header
